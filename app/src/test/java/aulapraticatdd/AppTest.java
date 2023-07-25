3b749fc5135ef6b570d6df6f355d1d8dbf5b7517
Bruno Silva <brunooliveirasilva77@gmail.com>
2023-07-24 22:38:22 -0300

    5° COMMIT & PUSH

--- app/src/test/java/aulapraticatdd/AppTest.java
+++ app/src/test/java/aulapraticatdd/AppTest.java
@@ -18,5 +18,19 @@ class AppTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+
+    }
+
 }
